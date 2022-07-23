b2ef1d7674ee4ccd8de38140df3ce85149da241e
Siddharth Agarwal <108752752+siddharth-1900290110110-internship@users.noreply.github.com>
2022-07-24 00:02:55 +0530

    Create 	Merge two sorted Arrays without extra space.cpp

---
+++ DAY-2/Merge two sorted Arrays without extra space.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int ar1[], int ar2[], int n, int m) {
+  
+  // code here 
+  int gap = ceil((float)(n + m) / 2);
+  
+  while (gap > 0) {
+    int i = 0;
+    
+    int j = gap;
+    while (j < (n + m)) {
+      
+      if (j < n && ar1[i] > ar1[j]) {
+        
+        swap(ar1[i], ar1[j]);
+      } else if (j >= n && i < n && ar1[i] > ar2[j - n]) {
+        
+        swap(ar1[i], ar2[j - n]);
+      } else if (j >= n && i >= n && ar2[i - n] > ar2[j - n]) {
+        
+        swap(ar2[i - n], ar2[j - n]);
+      }
+      
+      
+      j++;
+      i++;
+    }
+    
+    if (gap == 1) {
+      gap = 0;
+      
+    } else {
+      gap = ceil((float) gap / 2);
+    }
+  }
+}
