f6cf81228c4f2d5146822912f417b40ded050982
Siddharth Agarwal <108752752+siddharth-1900290110110-internship@users.noreply.github.com>
2022-07-28 23:17:23 +0530

    Create Two Sum.cpp

---
+++ DAY-4/Two Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    
+         unordered_map<int, int> m;
+         
+        for (int i = 0; i < nums.size(); i++) {
+        
+            int rem = target - nums[i];
+            
+            if (m.find(rem) != m.end())
+            
+                return {m[rem], i};
+            m[nums[i]] = i;
+        }
+        return {};
+    }
+};
