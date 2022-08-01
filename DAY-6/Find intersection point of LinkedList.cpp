a23bfca29951ca0e07b2111acc11ef5a69e14876
Siddharth Agarwal <108752752+siddharth-1900290110110-internship@users.noreply.github.com>
2022-08-01 23:33:53 +0530

    Create Find intersection point of LinkedList.cpp

---
+++ DAY-6/Find intersection point of LinkedList.cpp
@@ -0,0 +1,38 @@
+void insertNode(node* &head,int val) {
+    node* newNode = new node(val);
+    
+    if(head == NULL) {
+      
+        head = newNode;
+        return;
+    }
+    
+    node* temp = head;
+  
+    while(temp->next != NULL) temp = temp->next;
+    
+    temp->next = newNode;
+    return;
+}
+
+//utility function to check presence of intersection
+
+node* intersectionPresent(node* head1,node* head2) {
+  
+    while(head2 != NULL) {
+      
+        node* temp = head1;
+      
+        while(temp != NULL) {
+          
+            //if both nodes are same
+          
+            if(temp == head2) return head2;
+          
+            temp = temp->next;
+        }
+        head2 = head2->next;
+    }
+    //intersection is not present between the lists return null
+    return NULL;
+}
